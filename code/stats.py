0a996773a325df23c73e076088526192ea5a717a
Lennart Jern <lennart.jern@gmail.com>
2016-12-28 10:21:14 +0200

    Stats.py somewhat working

--- code/stats.py
+++ code/stats.py
@@ -16,64 +16,40 @@ import matplotlib.pyplot as plt
 
 def total_stats():
     """
-    Read the data from files and calculate statistical values and make plots.
+    Read the data from files, calculate statistical values and make plots.
     """
-    # The data file names are of the form data<thread count>.csv
-    base = "../data/data"
-    thread_base = "../data/threads"
-    ext = ".csv"
-    header = ("Normal", "Batch", "Idle", "FIFO", "RR")
-    # Data frames to store the results in
-    med = pd.DataFrame(columns=header)          # Medians (total runtime)
-    mx = pd.DataFrame(columns=header)           # Max (total runtime)
-    mn = pd.DataFrame(columns=header)           # Min (total runtime)
-    thread_med = pd.DataFrame(columns=header)   # Medians (threads)
-    thread_mx = pd.DataFrame(columns=header)    # Max (threads)
-    thread_mn = pd.DataFrame(columns=header)    # Min (threads)
 
-    # For each number of threads
-    for i in range(1,11):
-        # Build the file name
-        f = base + str(i) + ext             # Total run times
-        thr_f = thread_base + str(i) + ext  # Thread times
-        # Read the time data
-        df = pd.read_csv(f)
-        thr_df = pd.read_csv(thr_f)
-
-        # Calculate some statistical properties
-        med.loc[i] = df.median()
-        mx.loc[i] = df.max()
-        mn.loc[i] = df.min()
-        thread_med.loc[i] = thr_df.median()
-        thread_mx.loc[i] = thr_df.max()
-        thread_mn.loc[i] = thr_df.min()
-
-        # Plot and save some nice figures
-        # Density curves for thread count 2, 4, 6 and 8
-        if (i == 2 or i == 4 or i == 6 or i == 8):
-            ax = thr_df.plot.kde()
-            ax.set_xlabel("Time (s)")
-            fig = ax.get_figure()
-            fig.savefig("density"+str(i)+".pdf")
-
-        # Box plots for all thread counts
-        ax = thr_df.plot.box(figsize=(4.5,3))
-        ax.set_ylabel("Time (s)")
-        fig = ax.get_figure()
-        fig.savefig("box"+str(i)+".pdf")
-
-
-    # Calculate ranges
-    rng = mx-mn
-    thr_rng = mx-mn
-
-    # Write everything to files
-    data_frames = [med, mx, mn, thread_med, thread_mx, thread_mn, rng, thr_rng]
     base = "../data/"
-    names = ["medians", "max", "min", "thread_medians", "thread_max",
-             "thread_min", "range", "thread_range"]
-    for frm, name in zip(data_frames, names):
-        frm.to_csv(base+name + ext, index_label="Threads", float_format="%.5f")
+    total = "data"
+    read_times = "read_times"
+    ext = ".csv"
+    header = ("cfq", "deadline", "noop")
+
+    # Build the file name
+    f = base + total + ext               # Total run times
+    read_f = base + read_times + ext     # Thread times
+    # Read the time data
+    df = pd.read_csv(f, header=None)
+    df = df.transpose()
+    df = df[1:]
+    read_df = pd.read_csv(read_f)
+
+    # Calculate some statistical properties
+    df.describe().to_csv(base+"total_stats"+ext)
+    read_df.describe().to_csv(base+"read_stats"+ext)
+
+    # Plot and save some nice figures
+    # Density curves
+    ax = read_df.plot.kde()
+    ax.set_xlabel("Time (s)")
+    fig = ax.get_figure()
+    fig.savefig(base+"density.pdf")
+
+    # Box plots for all thread counts
+    ax = read_df.plot.box(figsize=(4.5,3))
+    ax.set_ylabel("Time (s)")
+    fig = ax.get_figure()
+    fig.savefig(base+"box.pdf")
 
 
 def collect_thread_times(file_name):
@@ -94,37 +70,22 @@ def collect_thread_times(file_name):
 
 
 def thread_stats():
-    """Collect timing information about all threads and store in csv files"""
-    threads = [i for i in range(1, 11)]
-    schedulers = ["n", "b", "i", "f", "r"]
-    base = "../data/threads"
+    """Collect timing information about all schedulers and store in csv files"""
+    schedulers = ["cfq", "deadline", "noop"]
+    base = "../data/"
     ext = ".log"
-    header=("Normal", "Batch", "Idle", "FIFO", "RR")
+    header=("cfq", "deadline", "noop")
 
-    # Collect all times for one thread count in one file
-    for t in threads:
-        times = {key: [] for key in schedulers}
-        for s in schedulers:
-            f = get_file_name(t, s)
-            times[s] = collect_thread_times(f)
-        # Write to file
-        df = pd.DataFrame(times)
-        df.to_csv(get_csv_name(t), index=False, header=header)
-
-
-def get_file_name(threads, scheduler):
-    """Get the file name for the data regarding <scheduler> and <threads>"""
-    base = "../data/threads"
-    ext = ".log"
-    return base + str(threads) + scheduler + ext
-
-def get_csv_name(threads):
-    """For a specific number of threads: Get name of file to write data to"""
-    base = "../data/threads"
-    ext = ".csv"
-    return base + str(threads) + ext
+    # Collect all times in one file
+    times = {key: [] for key in schedulers}
+    for s in schedulers:
+        f = base+s+ext
+        times[s] = collect_thread_times(f)
+    # Write to file
+    df = pd.DataFrame(times)
+    df.to_csv(base+"read_times.csv", index=False, header=header)
 
 # Collect thread timings
-thread_stats()
+#thread_stats()
 # Get statistics and plots
-total_stats()
+#total_stats()
